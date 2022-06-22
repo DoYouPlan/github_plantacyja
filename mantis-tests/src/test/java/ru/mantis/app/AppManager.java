8b986d417eff2e9743904fc02424e5afd5b44086
DoYouPlan <plan1337@yandex.ru>
2022-06-23 01:08:30 +0600

    Doyouplan
    масштабные покраснения

--- mantis-tests/src/test/java/ru/mantis/app/AppManager.java
+++ mantis-tests/src/test/java/ru/mantis/app/AppManager.java
@@ -1,92 +1,87 @@
-package ru.mantis.app;
+  package ru.mantis.app;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.remote.BrowserType;
+  import org.openqa.selenium.WebDriver;
+  import org.openqa.selenium.chrome.ChromeDriver;
+  import org.openqa.selenium.firefox.FirefoxDriver;
+  import org.openqa.selenium.ie.InternetExplorerDriver;
+  import org.openqa.selenium.remote.BrowserType;
 
-import java.util.concurrent.TimeUnit;
+  import java.io.File;
+  import java.io.FileReader;
+  import java.io.IOException;
+  import java.util.Properties;
+  import java.util.concurrent.TimeUnit;
+  public class AppManager {
+      private Properties properties;
+      WebDriver wd;
 
-import static org.testng.Assert.fail;
+      private String browser;
 
-public class AppManager {
-    private final StringBuffer verificationErrors = new StringBuffer();
-    public WebDriver driver;
-    private String baseUrl;
-    private JavascriptExecutor js;
-    // private static WebDriver driver = new ChromeDriver();
-    private boolean acceptNextAlert = true;
+      public AppManager(String browser) {
 
-    public void login(String UserName, String Password, String browtype) {
-        String browser = browtype;
-        if(browser == BrowserType.CHROME){
-            driver = new ChromeDriver();
-        }else if(browser == BrowserType.IE){
-            driver = new InternetExplorerDriver();
-        }else if(browser == BrowserType.OPERA){
-            driver = new OperaDriver();
-        }
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        js = (JavascriptExecutor) driver;
-        driver.get("http://localhost/addresbook/www/");
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys(UserName);
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys(Password);
-        driver.findElement(By.xpath("//input[@value='\u0412\u043E\u0439\u0442\u0438']")).click();
-        driver.findElement(By.linkText("\u0413\u0440\u0443\u043f\u043f\u044b")).click();
-    }
-    public void LoadInfo(String Text) {
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys(Text);
-        driver.findElement(By.name("group_header")).click();
-        if (!By.name("group_header").equals("null")) {
-            driver.findElement(By.name("group_header")).sendKeys("fuck");
-        }
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys(Text);
-    }
-    public void logout() {
-        driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-          fail(verificationErrorString);
-        }
-    }
+          this.browser = browser;
+          properties = new Properties();
 
-    private boolean isElementPresent(By by) {
-      try {
-        driver.findElement(by);
-        return true;
-      } catch (NoSuchElementException e) {
-        return false;
       }
-    }
 
-    private boolean isAlertPresent() {
-      try {
-        driver.switchTo().alert();
-        return true;
-      } catch (NoAlertPresentException e) {
-        return false;
-      }
-    }
+      public void init() throws IOException {
+          String target = System.getProperty("target", "local");
+          properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-    private String closeAlertAndGetItsText() {
-      try {
-        Alert alert = driver.switchTo().alert();
-        String alertText = alert.getText();
-        if (acceptNextAlert) {
-          alert.accept();
-        } else {
-          alert.dismiss();
-        }
-        return alertText;
-      } finally {
-        acceptNextAlert = true;
+          if (browser.equals(BrowserType.FIREFOX)) {
+              wd = new FirefoxDriver();
+          } else if (browser.equals(BrowserType.CHROME)) {
+              wd = new ChromeDriver();
+          } else if (browser.equals(BrowserType.IE)) {
+              wd = new InternetExplorerDriver();
+          }
+          wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+          wd.get(properties.getProperty("web.baseUrl"));
+
       }
-    }
-}
+
+      public void stop() {
+          wd.quit();
+      }
+
+      public HttpSession newSession() {
+        return  new HttpSession(this);
+      }
+
+
+      public String getProperty(String key) {
+          return properties.getProperty(key);
+      }
+  }
+
+
+
+
+//public class AppManager {
+//   private final StringBuffer verificationErrors = new StringBuffer();
+//   public WebDriver driver;
+//   private String baseUrl;
+// private JavascriptExecutor js;
+// private static WebDriver driver = new ChromeDriver();
+//  private boolean acceptNextAlert = true;
+
+//public void login(String UserName, String Password, String browtype) {
+//  String browser = browtype;
+//if(browser == BrowserType.CHROME){
+//  driver = new ChromeDriver();
+// }else if(browser == BrowserType.IE){
+//    driver = new InternetExplorerDriver();
+// }else if(browser == BrowserType.OPERA){
+//   driver = new OperaDriver();
+// }
+//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//js = (JavascriptExecutor) driver;
+
+//  }
+//  public void logout() {
+///    driver.quit();
+// String verificationErrorString = verificationErrors.toString();
+//  if (!"".equals(verificationErrorString)) {
+//  fail(verificationErrorString);
+//   }
+// }
--- mantis-tests/src/test/java/ru/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/mantis/tests/TestBase.java
@@ -1,21 +1,21 @@
 package ru.mantis.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import ru.mantis.app.AppManager;
 
 public class TestBase {
 
     protected final AppManager app = new AppManager();
 
-    @BeforeClass(alwaysRun = true)
+    @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
      // System.setProperty("webdriver.chrome.driver", "path to chromedriver");
         app.login("admin", "secret", BrowserType.CHROME);
     }
 
-    @AfterClass(alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
         app.logout();
     }
